de770684330803efd592de09cd7103f912173298
Luigi Oliveira <luigi@elti.com.br>
2020-09-14 21:45:26 -0300

    Códigos Iniciais

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# mouse-head
-Projeto que tem como objetivo trazer mais acessibilidade para as aplicações web no geral. O intuito da biblioteca é fazer com que um usuário que não possui os braços consiga mover o mouse usando sua cabeça.
+# Move Mouse Head
+
+[![N|Solid](https://uploaddeimagens.com.br/images/002/869/762/original/DesenvolvidoPor.png)](https://8pixel.com.br)
+
+O Move mouse é uma aplicação que tem como principal objetivo tornar os sites acessíveis para pessoas que tem algum tipo de deficiência motora e por isso não consegue mover o mouse com a mão ou com o pé. 
+O Objetivo dele é bem simples, com movimentos com a cabeça que são reconhecidos pela webCam, o usuário vai conseguir mover o mover o mouse e efetuar cliques.
+
+  - Inclinando a cabeça para cima o mouse vai para cima 
+  - Inclinando a cabeça para  baixo o mouse vai para baixo
+  - Inclinando a cabeça para a esquerda o mouse vai para esquerda
+  - Inclinando a cabeça para a direita o mouse vai para a direita
+  
+# Como funciona
+
+  - Import a HTML file and watch it magically convert to Markdown
+  - Drag and drop images (requires your Dropbox account be linked)
+
+
+
+### Plugins
+
+Para funcionar o Move mouse utiliza ferramentas e plugins:
+
+* [Teachable Machine] - Uma maneira rápida e fácil de criar modelos de aprendizado de máquina para seus sites, aplicativos e muito mais - sem necessidade de conhecimento ou codificação.
+* [Tensor Flow] - Desenvolva modelos de ML em JavaScript e use ML diretamente no navegador ou no Node.js.
+
+E, claro, o próprio Move Mouse é open source com um [repositório público][dill]
+no GitHub.
+
+### Instalação
+
+Para instalar é muito simples, basta efetuar o download ou o clone do projeto e inserir o uma tag <script> na <head> do seu website.
+
+```html
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta  charset="UTF-8">
+	    <!-- META TAGS DO SEU WEBSITE -->
+	    <title>[NO DO SEU WEBSITE]</title>
+        <script type="text/javascript" src="lib.js"></script>
+    </head>
+    <body>
+	</body>
+</html>
+```
+A tag pode ser inserida também ao final do body.
+
+
+### Disparar e usar
+
+Para disparar o evento e começar a utilizar a biblioteca para mover o mouse, basta adicionar a o atributo data-head-mouse em qualquer o botão.
+```html
+<button type="button" data-head-mouse>Começar</button>
+```
+
+### Demo
+Para ver em funcionamento [visite nossa página demo]
+
+### Desenvolvimento
+
+Esta aplicação ainda esta em desenvolvimento, então toda e qualquer ajuda é bem vinda.
+
+### Todos
+
+ - Realizar funcional com as personas corretas
+ - Adicionar méodulo de controle de velocidade de mouse
+ - Adicionar méodulo de controle de velocidade de mouse
+ 
+
+License
+----
+
+MIT
+
+
+** Software livre**
+
+[//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
+
+   [Tensor Flow]: <https://www.tensorflow.org/js>
+   [ visite nossa página demo]: <https://demowebpage.8pixel.com.br>
+   [Teachable Machine]: <https://teachablemachine.withgoogle.com/>
---
+++ head-mouse.js
@@ -0,0 +1,179 @@
+    
+    // Caregando arquivos para o uso da biblioteca
+    let tfMin = document.createElement('script');
+    
+    tfMin.src = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js";
+    document.getElementsByTagName('head')[0].appendChild(tfMin);
+    
+    let teachablemachine = document.createElement('script');
+
+    teachablemachine.src = "https://cdn.jsdelivr.net/npm/@teachablemachine/pose@0.8/dist/teachablemachine-pose.min.js";
+
+    document.getElementsByTagName('head')[0].appendChild(teachablemachine);    
+    
+    var x, y; 
+    var px, py; 
+    px = py = 0; 
+
+    // Elemento que será utilizado com cursos 
+    var cursor = document.createElement('img');
+    cursor.src = "https://media.geeksforgeeks.org/wp-content/uploads/20200319212118/cursor2.png";
+    cursor.width = 15;
+    cursor.height = 20;
+    cursor.classList.add('head-mouse-cursor');
+    document.getElementsByTagName('body')[0].appendChild(cursor);
+    
+    
+
+    /* O mutex é usado para evitar vários eventos de clique de
+        disparando ao mesmo tempo devido à posição diferente
+        do cursor */ 
+    var mutex = false; 
+
+    /* 
+     O evento a seguir está selecionando o elemento
+      no cursor da imagem e dispara um clique na posição dele.
+      O evento a seguir é acionado apenas quando o mouse é pressionado.
+     */ 
+    window.addEventListener("mouseup", function(e) { 
+        
+        // Coloca o objeto na posição do cursor da imagem 
+        var tmp = document.elementFromPoint(x + px, y + py);  
+        mutex = true; 
+        tmp.click(); 
+        cursor.style.left = (px + x) + "px"; 
+        cursor.style.top = (py + y) + "px"; 
+    }) 
+
+    /* Fazendo a imagem acompanhar o movimento comum do mouse */ 
+    window.addEventListener("mousemove", function(e) { 
+
+        // Recuperando posição do mouse  
+        x = e.clientX; 
+        y = e.clientY; 
+
+        // Atribuindo margens a imagem para acompanhar a movimentação do mouse. 
+        cursor.style.left = (px + x) + "px"; 
+        cursor.style.top = (py + y) + "px"; 
+
+    }); 
+
+        
+    // More API functions here:
+    // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
+
+    // the link to your model provided by Teachable Machine export panel
+    const URL = "https://teachablemachine.withgoogle.com/models/RufFlH_7F/";
+    let model, webcam, ctx, labelContainer, maxPredictions;
+
+    document.querySelector('*[head-mouse]').onclick = function init() {
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
+
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // Note: the pose library adds a tmPose object to your window (window.tmPose)
+        model = await tmPose.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+
+        // Convenience function to setup a webcam
+        const size = 200;
+        const flip = true; // whether to flip the webcam
+        webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        await webcam.play();
+        window.requestAnimationFrame(loop);
+
+        // append/get elements to the DOM
+        const canvas = document.getElementById("canvas");
+        canvas.width = size; canvas.height = size;
+        ctx = canvas.getContext("2d");
+        labelContainer = document.getElementById("label-container");
+        for (let i = 0; i < maxPredictions; i++) { // and class labels
+            labelContainer.appendChild(document.createElement("div"));
+        }
+    }
+
+    async function loop(timestamp) {
+        webcam.update(); // update the webcam frame
+        await predict();
+        window.requestAnimationFrame(loop);
+    }
+
+    async function predict() {
+        // Prediction #1: run input through posenet
+        // estimatePose can take in an image, video or canvas html element
+        const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+        // Prediction 2: run input through teachable machine classification model
+        const prediction = await model.predict(posenetOutput);
+
+
+        for (let i = 0; i < prediction.length; i++) {
+            const classPrediction =
+           	 prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+            labelContainer.childNodes[i].innerHTML = classPrediction;
+
+            console.log(prediction[i]);
+
+            if(prediction[i].probability > 0.95){
+
+
+ 				var cursor = document.getElementById("cursor");  
+ 				var cursorTop = cursor.offsetTop;
+ 				var cursorTop = cursor.offsetLeft;
+				
+				var marginLeft =parseInt(cursor.style.left);
+				var marginTop =parseInt(cursor.style.top);
+
+
+                var tmp = document.elementFromPoint(marginLeft, marginTop);  
+                mutex = true; 
+
+ 				switch(prediction[i].className){
+
+ 					case 'Esquerda': 
+ 						marginLeft-= 5;
+ 					break;
+
+ 					case 'Direita':
+
+ 						marginLeft+=5;
+ 					break;
+
+
+ 					case 'Baixo':
+ 						marginTop+=5;
+ 					break;
+
+
+ 					case 'Cima':
+ 						marginTop-=5;
+ 					break;
+                        
+ 					case 'Clique':
+            			tmp.click(); 
+ 					break;
+ 				}
+
+
+ 				cursor.style.left = marginLeft + 'px';
+ 				cursor.style.top = marginTop + 'px';
+            }
+        }
+
+        // finally draw the poses
+        drawPose(pose);
+    }
+
+    function drawPose(pose) {
+        if (webcam.canvas) {
+            ctx.drawImage(webcam.canvas, 0, 0);
+            // draw the keypoints and skeleton
+
+            if (pose) {
+                const minPartConfidence = 0.5;
+                tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+                tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+            }
+        }
+    }
